a4aa00c21a7297012ca721bc6c1d5cfe297d2e5c
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-07 04:37:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeons = {}
+ 
+  data[:gender].each do |key, value|
+    value.map do | item |
+        pigeons[item] = {}
+        pigeons[item][:color] = []
+        pigeons[item][:gender] = []
+        pigeons[item][:lives] = []
+    end
+  end
+  
+  data[:color].each do |key, value|
+    value.map do |item|
+      pigeons[item][:color] << key.to_s
+    end
+  end
+  
+  data[:gender].each do |key, value|
+    value.map do |item|
+      pigeons[item][:gender] << key.to_s
+    end
+  end
+  
+  data[:lives].each do |key, value|
+    value.map do |item|
+      pigeons[item][:lives] << key.to_s
+    end
+  end
+  
+  pigeons
+  
 end
